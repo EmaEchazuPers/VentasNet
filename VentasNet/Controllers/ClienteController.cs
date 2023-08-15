5f61ba40f1936b88741484e6e49d9d1b657f6b1f
EmaEchazuPers <ema.echazu2@gmail.com>
2023-08-14 22:57:57 -0300

    Agregado de lista

--- VentasNet/Controllers/ClienteController.cs
+++ VentasNet/Controllers/ClienteController.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VentasNet.Models;
 
 namespace VentasNet.Controllers
 {
     public class ClienteController : Controller
-    {
+    {   
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Listado() 
+        {
+            List<Cliente> listadoCliente = new List<Cliente>();
+            listadoCliente.Add(new Cliente() { Id = 1, RazonSocial = "Empresa 1", Cuit = "66473828922", Domicilio = "Centro", Provincia = "Salta" });
+
+            //ViewBag es una clase genérica que me acepta lo que le pasemos
+
+            ViewBag.Cliente = listadoCliente; 
             return View();
         }
     }
---
+++ VentasNet/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace VentasNet.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string? RazonSocial { get; set; } //Se agrega ? para permitir que sea nulo el campo
+        public string? Cuit { get; set; }
+        public string? Domicilio { get; set; }
+        public string? Provincia { get; set; }
+
+    }
+}
